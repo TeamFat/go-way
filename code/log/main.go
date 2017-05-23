15987db263a0b92217db6bf281aca17a484cf38d
chenjie <chenjiesuper@163.com>
2017-05-24 01:08:07 +0800

    log

---
+++ code/log/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/astaxie/beego/logs"
+)
+
+func main() {
+	//an official log.Logger
+	l := logs.GetLogger()
+	l.Println("this is a message of http")
+	//an official log.Logger with prefix ORM
+	logs.GetLogger("ORM").Println("this is a message of orm")
+
+	logs.Debug("my book is bought in the year of ", 2016)
+	logs.Info("this %s cat is %v years old", "yellow", 3)
+	logs.Warn("json is a type of kv like", map[string]int{"key": 2016})
+	logs.Error(1024, "is a very", "good game")
+	logs.Critical("oh,crash")
+	log := logs.NewLogger()
+	log.SetLogger(logs.AdapterConsole)
+	log.Debug("this is a debug message")
+}
