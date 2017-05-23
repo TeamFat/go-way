477144eabff8cc60bf936188b53b404932ebc638
chenjie <chenjiesuper@163.com>
2017-05-24 01:10:49 +0800

    log stdout

--- code/log/main.go
+++ code/log/main.go
@@ -19,4 +19,12 @@ func main() {
 	log := logs.NewLogger()
 	log.SetLogger(logs.AdapterConsole)
 	log.Debug("this is a debug message")
+
+	// 2017/05/24 01:07:34 [ORM] this is a message of orm
+	// 2017/05/24 01:07:34 [D] my book is bought in the year of  2016
+	// 2017/05/24 01:07:34 [I] this yellow cat is 3 years old
+	// 2017/05/24 01:07:34 [W] json is a type of kv like map[key:2016]
+	// 2017/05/24 01:07:34 [E] 1024 is a very good game
+	// 2017/05/24 01:07:34 [C] oh,crash
+	// 2017/05/24 01:07:34 [D] this is a debug message
 }
