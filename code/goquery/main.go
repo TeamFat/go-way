b8517cea30252e24de46f17602e1807eaad7b9d6
chenjie <chenjiesuper@163.com>
2017-05-25 00:58:31 +0800

    goquery

---
+++ code/goquery/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func GetNews() {
+	doc, err := goquery.NewDocument("https://gocn.io/")
+	if err != nil {
+		fmt.Println(err)
+	}
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		match := "每日新闻"
+		if strings.Contains(s.Text(), match) {
+			url, _ := s.Attr("href")
+			fmt.Println(match, url)
+			os.Exit(0)
+		}
+	})
+}
+func main() {
+	GetNews()
+}
