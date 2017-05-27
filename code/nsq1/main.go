0d70acc9795461d0f00f50ae9782c278b1b4beda
chenjie <chenjiesuper@163.com>
2017-05-28 03:45:53 +0800

    nsqlookupd

---
+++ code/nsq1/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/nsqio/go-nsq"
+)
+
+func main() {
+	go startConsumer()
+	startProducer()
+}
+
+var url string
+var url1 string
+
+func init() {
+	flag.StringVar(&url, "url", "127.0.0.1:4150", "nsqd")
+	flag.StringVar(&url1, "url1", "127.0.0.1:4161", "nsqlookupd")
+
+	flag.Parse()
+}
+
+// 生产者
+func startProducer() {
+	cfg := nsq.NewConfig()
+	producer, err := nsq.NewProducer(url, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 发布消息
+	for {
+		if err := producer.Publish("test", []byte("test message")); err != nil {
+			log.Fatal("publish error: " + err.Error())
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// 消费者
+func startConsumer() {
+	cfg := nsq.NewConfig()
+	consumer, err := nsq.NewConsumer("test", "sensor01", cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 设置消息处理函数
+	consumer.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		log.Println(string(message.Body))
+		return nil
+	}))
+	// nsqlookupd
+	if err := consumer.ConnectToNSQLookupds([]string{url1}); err != nil {
+		log.Fatal(err)
+	}
+	<-consumer.StopChan
+}
