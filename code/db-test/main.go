039f5e517ffa355181ed3ffa5802dbb351c02dd1
chenjie <chenjiesuper@163.com>
2017-06-06 00:37:35 +0800

    goroutine

--- code/db-test/main.go
+++ code/db-test/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"db-test/db"
-	"db-test/task"
+	"time"
+
+	"github.com/chenjiesuper/learning-golang/code/db-test/db"
+	"github.com/chenjiesuper/learning-golang/code/db-test/task"
 )
 
 func main() {
 	db.ConnRedis()
-	task.Run()
+	go task.Run()
+	//go task.RunT()
+	//不支持并发，需要改成redis连接池
+	time.Sleep(time.Second * 3)
 	db.StopRedis()
 }
--- code/db-test/task/task.go
+++ code/db-test/task/task.go
@@ -1,14 +1,20 @@
 package task
 
 import (
-	"db-test/db"
 	"fmt"
 
+	"github.com/chenjiesuper/learning-golang/code/db-test/db"
 	"github.com/garyburd/redigo/redis"
 )
 
 func Run() {
 	db.C.Do("SET", "hello", "world")
 	s, _ := redis.String(db.C.Do("GET", "hello"))
+	fmt.Printf("%#v\n", s)
+}
+
+func RunT() {
+	db.C.Do("SET", "helloT", "world")
+	s, _ := redis.String(db.C.Do("GET", "helloT"))
 	fmt.Printf("%#v\n", s)
 }
