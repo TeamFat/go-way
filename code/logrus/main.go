fababab09c32793c518e0ab0b9eb3a743e70f041
chenjie <chenjiesuper@163.com>
2017-07-06 23:27:14 +0800

    logs

---
+++ code/logrus/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/logmatic/logmatic-go"
+)
+
+func main() {
+	// use JSONFormatter
+	log.SetFormatter(&logmatic.JSONFormatter{})
+	// log an event as usual with logrus
+	log.WithFields(log.Fields{"string": "foo", "int": 1, "float": 1.1}).Info("My first ssl event from golang")
+	contextLogger := log.WithFields(log.Fields{
+		"common": "XXX common content XXX",
+		"other":  "YYY special context YYY",
+	})
+
+	contextLogger.Info("AAAAAAAAAAAA")
+	contextLogger.Info("BBBBBBBBBBBB")
+}
+
+//http://www.jianshu.com/p/a855e91c5335
---
+++ code/logs/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.OpenFile("test.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	log.Println("==========works==============")
+}
