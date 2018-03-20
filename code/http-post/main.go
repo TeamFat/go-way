2f57820b850789564fb5c92d5abb6be54507e9ea
chenjie <chenjie19@jd.com>
2018-03-20 19:54:10 +0800

    fix

---
+++ code/http-post/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	httpPost()
+}
+func httpPost() {
+	resp, err := http.Post("http://www.01happy.com/demo/accept.php",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("name=cjb"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
