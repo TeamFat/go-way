31179ed7931e16e0aeacd7ddaa7c91cdf5657e9d
chenjie <chenjie19@jd.com>
2018-04-28 12:04:38 +0800

    kafka test and some link in doc

---
+++ code/kafka/doc.go
@@ -0,0 +1,9 @@
+// java 源码安装 https://blog.csdn.net/fenglailea/article/details/26006647
+
+// kafka https://segmentfault.com/a/1190000012730949
+
+// zk https://www.jianshu.com/p/9d48a5bd1669
+
+// go-kafka http://www.cnblogs.com/lanyangsh/p/7782771.html
+
+// 问题 https://blog.csdn.net/sahusoft/article/details/7388617
---
+++ code/kafka/producer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	topic := "myTopic"
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	// Wait for message deliveries
+	p.Flush(15 * 1000)
+}
