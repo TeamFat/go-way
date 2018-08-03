538a74b4f24583c3acef8160eb70f5e787accf4c
jay chen <chenjiesuper@gmail.com>
2018-08-03 23:44:59 +0800

    pprof

--- code/atomic/main.go
+++ code/atomic/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"runtime"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 var (
@@ -13,11 +17,16 @@ var (
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:8081", nil))
+	}()
 	wg.Add(2)
 	go incCount(1)
 	go incCount(2)
 	wg.Wait()
 	fmt.Println("dfdadf", count)
+	time.Sleep(time.Minute)
+
 }
 
 func incCount(ii int32) {
