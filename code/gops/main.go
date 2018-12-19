8ac7da61bb9440396aeb19aac40327b07a41c6de
jay chen <chenjiesuper@gmail.com>
2018-12-19 19:05:35 +0800

    gops

---
+++ code/gops/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/google/gops/agent"
+)
+
+func main() {
+	if err := agent.Listen(agent.Options{}); err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Hour)
+}
